b3dd192f77bc9f8eb5b3c2d466643d8c0c80da54
Stakh Jurii <stakh.jurii@gmail.com>
2019-03-06 09:52:58 +0200

    First Commit

---
+++ crc/test/Player/Player.java
@@ -0,0 +1,36 @@
+package src.test.Player;
+import java.util.Scanner;
+
+public class Player {
+
+	int number;
+
+
+	public static Player getPlayer(){
+
+		Player a = new Player();
+
+		return a;
+	}
+
+
+public int makeAChoiсe (){
+
+Scanner scan = new Scanner(System.in);
+
+this.namber = (int) (Math.random() * 3 );
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
